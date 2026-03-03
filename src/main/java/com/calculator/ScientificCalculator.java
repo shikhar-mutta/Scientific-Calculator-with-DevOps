7ee6a4b5d97ae360de33e7c0ac5a8bbf1949f4ee
Shikhar Mutta <shikharmutta67@gmail.com>
2026-03-04 00:47:11 +0530

    fail test 1

--- src/main/java/com/calculator/ScientificCalculator.java
+++ src/main/java/com/calculator/ScientificCalculator.java
@@ -22,9 +22,9 @@ public class ScientificCalculator {
     /**
      * Returns the sum of two numbers.
      */
-    public static double add(double a, double b) {
-        return a + b;
-    }
+    // public static double add(double a, double b) {
+    //     return a + b;
+    // }
 
     /**
      * Returns the difference of two numbers (a - b).
